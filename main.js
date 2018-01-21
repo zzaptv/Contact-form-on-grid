6b20da6ba5ab6adefc98306e9c7ca243006dde92
zzaptv <thezzaptv@gmail.com>
2018-01-21 19:13:20 +0100

    Firebase back end

---
+++ main.js
@@ -0,0 +1,60 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBAX1VKFu30I9z_ME7-ev810UoVTpcQMyg",
+    authDomain: "contact-form-4dfd0.firebaseapp.com",
+    databaseURL: "https://contact-form-4dfd0.firebaseio.com",
+    projectId: "contact-form-4dfd0",
+    storageBucket: "contact-form-4dfd0.appspot.com",
+    messagingSenderId: "1044491242077"
+  };
+  firebase.initializeApp(config);
+
+  //Reference messages collections
+  var messagesRef = firebase.database().ref('messages');
+
+
+  //Listen for form submit
+document.getElementById('contactform').addEventListener('submit', submitform);
+
+function submitform (e) {
+    e.preventDefault();
+ 
+    
+//Get values
+var name = getInputVal('name');
+var company = getInputVal('company');
+var email = getInputVal('email');
+var phone = getInputVal('phone');
+var message = getInputVal('message');
+
+//save message
+saveMessage(name, company, email, phone, message);
+
+//show alert
+document.querySelector('.alert').style.display='block';
+//hide alert after 3 seconds
+setTimeout(function() {
+    document.querySelector('.alert').style.display= 'none';
+}, 5000);
+
+
+document.getElementById('contactform').reset();
+}
+
+
+//function to get from values
+function getInputVal (id) {
+    return document.getElementById(id).value;
+}
+
+//save the massege to firebase
+function saveMessage(name, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set( {
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
+}
